eb6859b27386d65834517a4a3cca8fd1860a2676
Marcin Pająk <39952424+pelemistrz@users.noreply.github.com>
2024-11-02 10:30:48 +0100

    Add days

--- src/main/java/com/app/controller/MainWindowController.java
+++ src/main/java/com/app/controller/MainWindowController.java
@@ -7,8 +7,12 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
@@ -19,8 +23,16 @@ public class MainWindowController extends BaseController implements Initializabl
     private ChoiceBox<String> choiceBoxLocalCountry;
     @FXML
     private ChoiceBox<String> choiceBoxDestinationCountry;
-
-
+    @FXML
+    private Label todayLabel;
+    @FXML
+    private Label todayPlu2;
+    @FXML
+    private Label todayPlus1;
+    @FXML
+    private Label todayPlus3;
+    @FXML
+    private Label todayPlus4;
     @FXML
     void checkBtn() {
         return;
@@ -34,10 +46,17 @@ public class MainWindowController extends BaseController implements Initializabl
     public void initialize(URL url, ResourceBundle resourceBundle) {
        LoadCsv loadCsv = new LoadCsv();
        loadCsv.loadFromFile("src/main/resources/data.csv");
-
         choiceBoxLocalCountry.setItems(loadCsv.getCountriesList());
         choiceBoxDestinationCountry.setItems(loadCsv.getCountriesList());
         choiceBoxLocalCountry.setValue("Poland");
         choiceBoxDestinationCountry.setValue("Egypt");
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        todayLabel.setText(date.format(formatter));
+        todayPlus1.setText(date.plusDays(1).format(formatter));
+        todayPlu2.setText(date.plusDays(2).format(formatter));
+        todayPlus3.setText(date.plusDays(3).format(formatter));
+        todayPlus4.setText(date.plusDays(4).format(formatter));
+
     }
 }
